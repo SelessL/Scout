0dd18ab3a33a20534a193237c20dd6704e2935b5
Slautz <s.n.lautzenhiser@gmail.com>
2020-08-22 11:16:22 -0400

    seless is doing some weird stuff sorry -_-

---
+++ script.js
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+console.log(await cp.global());
+   
+>>>>>>> Stashed changes
+=======
+console.log(await cp.global());
+   
+>>>>>>> Stashed changes
