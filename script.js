022e90cf44455cbafdc9a6b6ca7db48fbafd0fcd
Soua Xiong <soua.p.xiong@hotmail.com>
2020-08-22 12:10:34 -0400

    Update

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+var APIkey = "9a690d0104msh334156481842de6p13b3a3jsn98714ffd81ea"
+var queryURL = "https://coinpaprika1.p.rapidapi.com/exchanges=" + APIkey;
 
-  var queryURL = "https://github.com/jaggedsoft/coinpaprika-js"; 
-  var currencyEl = "currency"
-  var SubmitBtn = "submit"
-  var date = "timeframe"
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://coinpaprika1.p.rapidapi.com/exchanges",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coinpaprika1.p.rapidapi.com",
+		"x-rapidapi-key": "9a690d0104msh334156481842de6p13b3a3jsn98714ffd81ea"
+	}
+}
 
-  
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
