5a01126d16f0637c99cc99c501c86807e271c27c
Soua Xiong <soua.p.xiong@hotmail.com>
2020-08-29 14:05:27 -0400

    trial

--- script.js
+++ script.js
@@ -79,7 +79,11 @@ const getPrice = (a) => {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
             label: 'Bitcoin',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [{
+              BCHPrice
+              "Time:"
+
+            }],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
